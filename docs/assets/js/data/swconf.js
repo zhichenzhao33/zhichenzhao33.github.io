96755d48d87c8ea7375db31a8ffe0ea6fbb997a9
luozhia <zhichen@luozhiadeMacBook-Air.local>
2026-01-17 13:12:38 -0800

    update

---
+++ docs/assets/js/data/swconf.js
@@ -0,0 +1,30 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1768684318',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/Paper/',
+      
+        '/Teaching/',
+      
+        '/Conferences/',
+      
+        '/timeline/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
